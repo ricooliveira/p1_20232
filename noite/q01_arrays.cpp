1f086c415993c1254a5a1aca3007817b7fc2afd5
ricooliveira <ricooliveira@gmail.com>
2023-08-16 19:43:23 -0300

    Aula16082023

---
+++ noite/q01_arrays.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+
+using namespace std;
+
+void ler_vetor(int vet[], int tam){
+    for(int i=0;i<tam;i++){
+        cout<<"Posicao "<<i+1<<": ";
+        cin>>vet[i];
+    }
+}
+
+void imprimir_vetor(int vet[],int tam){
+    for(int i=0;i<tam;i++){
+        cout<<vet[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int maior_elemento(int vet[],int tam){
+    int maior=vet[0];
+    for(int i=1;i<tam;i++){
+        if(vet[i] > maior){
+            maior = vet[i];
+        }
+    }
+    return maior;
+}
+
+int menor_elemento(int vet[],int tam){
+    int menor=vet[0];
+    for(int i=1;i<tam;i++){
+        if(vet[i] < menor){
+            menor = vet[i];
+        }
+    }
+    return menor;
+}
+
+int somar_vetor(int vet[],int tam){
+    int soma = 0;
+    for(int i=0;i<tam;i++){
+        soma += vet[i];
+    }
+    return soma;
+}
+
+double media(int vet[],int tam){
+    return (double)somar_vetor(vet,tam) / tam;
+}
+
+int main(){
+    const int tam = 5;
+    int vetor[tam];
+    ler_vetor(vetor,tam);
+    imprimir_vetor(vetor,tam);
+    cout<<"Maior valor: "<<maior_elemento(vetor,tam)<<endl;
+    cout<<"Menor valor: "<<menor_elemento(vetor,tam)<<endl;
+    cout<<"Soma dos elementos: "<<somar_vetor(vetor,tam)<<endl;
+    cout<<"Media: "<<media(vetor,tam)<<endl;
+    return 0;
+}
